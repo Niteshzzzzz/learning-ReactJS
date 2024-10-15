20553bc269c59c9ae0fde650f3947cee31ade3c7
Nitesh Kumar <niteshkumarnkzzzzz@gmail.com>
2024-10-16 00:32:29 +0530

    React useState Hook

---
+++ React useState Hook/app.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import AppleBasket from "./Components/AppleBasket";
+import Button from "./Components/Button";
+import './style.css'
+import {createRoot} from 'react-dom/client'
+
+const App = () => {
+    let allApples = 10;
+
+    const [rightApples, setRightApples] = useState(0);
+
+    const [leftApples, setLeftApples] = useState(allApples-rightApples);
+
+    let leftEvent = () => {
+        if (rightApples>0) {
+            setLeftApples(leftApples+1);
+            setRightApples(rightApples-1);
+        }
+       
+    }
+    let rightEvent = () => {
+        if (leftApples>0) {
+            setLeftApples(leftApples-1);
+            setRightApples(rightApples+1);
+        }    }
+
+    return (
+        <div className="main">
+            <AppleBasket count={leftApples} basketName="Basket 1"/>
+            <Button clickHendler={leftEvent} arrow="&#8676;" title="left arrow" className="btn"/>
+            <Button clickHendler={rightEvent} arrow="&#8677;" title="Right arrow" className="btn"/>
+            <AppleBasket count={rightApples} basketName="Basket 2" />
+        </div>
+
+    )
+}
+
+export default App;
